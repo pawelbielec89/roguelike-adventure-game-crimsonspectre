faa78db8882ba8a20da227ee68c769deac814555
edobkowski <edobkowski@gmail.com>
2018-01-30 11:29:06 +0100

    initialisation of Character class

--- player_module.py
+++ player_module.py
@@ -14,5 +14,32 @@ class Character:
         self.damage = damage
         self.dexterity = dexterity
     
-    
+    def move(self, board):
+        repeat = True
+
+        while repeat:
+            repeat = False
+            direction = common.getch()
+            new_x = self.x_coord
+            new_y = self.y_coord
+
+            if direction == 'w':    # do the following statements if conditions are fullfilled
+                new_x -= 1  #decrementation of new_y varible
+            elif direction == 's':  # do the following statements if conditions are fullfilled
+                new_x += 1  # incrementation of new_x varible
+            elif direction == 'a':  # do the following statements if conditions are fullfilled
+                new_y -= 1  #decrementation of new_y varible
+            elif direction == 'd':  # do the following statements if conditions are fullfilled
+                new_y += 1  # incrementation of new_y varible
+            elif direction == 'q':  # do the following statements if conditions are fullfilled
+                exit(0) # call funtions exit with argument 0 to quit the program
+            
+            is_collision = board.check_collision(new_x, new_y)
+
+            if is_collision:
+                repeat = True
+            else:
+                board.update_board(self.x_coord, self.y_coord, new_x, new_y)
+                self.x_coord = new_x
+                self.y_coord = new_y
         
